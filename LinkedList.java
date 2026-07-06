66e7da98373d942a12260495f740df5b7c578268
abhisheksingla2005 <abhisheksingla2005@gmail.com>
2026-07-06 14:39:20 +0530

    Linked List

---
+++ LinkedList.java
@@ -0,0 +1,46 @@
+//write code to add node in front and in last in a linkedlist
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    // Method to add a new node at the end of the linked list
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to add a new node at the front of the linked list
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Method to display the linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
